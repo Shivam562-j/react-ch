30daa4404ee06e89a01d4be40009489b4969cf88
Shivam Kumar <76731566+Shivam562-j@users.noreply.github.com>
2023-11-29 21:37:11 +0530

    Add files via upload

---
+++ 00top-course/src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import {apiUrl, filterData} from "./data"
+import Navbar from './components/Navbar'
+import Filter from './components/Filter'
+import Cards from './components/Cards'
+import {toast} from 'react-toastify'
+import Spinner from './components/Spinner'
+
+function App() {
+
+  const [courses, setCourses] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+
+  useEffect( ()=> {
+    const fetchData = async() => {
+      setLoading(true);
+      try{
+        const res = await fetch(apiUrl);
+        const output = await res.json();
+        // save data into a variable
+        // console.log(output);
+        setCourses(output.data);
+      }
+      catch(error){
+        toast.error("Something went wrong");
+      }
+      setLoading(false);
+    }
+    fetchData();
+  }, [])
+
+  return (
+    <div className='min-h-screen flex flex-col bg-bgDark2'>
+      <div>
+        <Navbar />
+      </div>
+
+      <div>
+        <Filter 
+          category={category}
+          setCategory={setCategory}
+          filterData={filterData}
+        />
+      </div>
+      <div className='w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]'>
+        {
+          loading ? (<Spinner />) : (<Cards courses={courses} category={category}/> )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ 00top-course/src/components/Cards.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Card from "./Card";
+
+function Cards(props) {
+    let courses = props.courses;
+    let category = props.category;
+
+    const [likedCourses, setLikedCourses] = useState([]);
+
+    // returns you a list of all courses received from the api response
+    const getCourses = () => {
+        if (category === "All") {
+            let allCourses = [];
+            Object.values(courses).forEach((courseCategory) => {
+                courseCategory.forEach((course) => {
+                    allCourses.push(course);
+                });
+            });
+
+            return allCourses;
+        }
+        else{
+          return courses[category];
+        }
+    };
+
+    return (
+        <div className="flex flex-wrap justify-center gap-4 mb-4">
+            {getCourses().map((course) => {
+                return (
+                    <Card
+                        course={course}
+                        key={course.id}
+                        likedCourses={likedCourses}
+                        setLikedCourses={setLikedCourses}
+                    />
+                );
+            })}
+        </div>
+    );
+}
+
+export default Cards;
